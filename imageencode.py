a4d32edb5f6756d8d7783c1ebc3c66278a6c9062
Nicholas Kwon <nichkwon@gmail.com>
2015-04-04 17:20:53 -0500

    hackathon over

--- imageencode.py
+++ imageencode.py
@@ -10,7 +10,7 @@ from io import BytesIO
 from itertools import izip_longest # for Python 2.x
 import urllib
 
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+template_dir = os.path.dirname(__file__)
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 client_id = 'f01e2c9c566d815'
@@ -34,6 +34,13 @@ class Handler(webapp2.RequestHandler):
 class Home(Handler):
     def get(self):
         self.render('home.html')
+
+def url_to_img(url):
+    try:
+        handle = urllib.urlopen(url)
+        return Image.open(BytesIO(bytearray(handle.read())))
+    except:
+        return None
 
 class Encode(Handler):
     """Create a file.
@@ -62,7 +69,7 @@ class Encode(Handler):
         return new_val
 
     def encode(self, message, image, password="password"):
-        if len(password)==0:
+        if password == None or len(password)==0:
             password="password"
         binary_message = ''.join('{:08b}'.format(ord(c)) for c in password)
         binary_message += ''.join('{:08b}'.format(ord(c)) for c in message)
@@ -85,24 +92,28 @@ class Encode(Handler):
     def post(self):
         if self.request.get('pic'):
             img = Image.open(BytesIO(self.request.get('pic')))
-            msg = self.request.get('msg')
-            resp = dict()
-            if img and msg:
-                filename = '/imageencode/' + str(int(round(time.time() * 1000))) + '.png'
-                # Encodes the text into the image
-                print 'encoding'
-                self.encode(msg, img)
-                print 'just encoded'
-                # Creates a file with the given name
-                self.make_file(filename, img)
-                link = 'http://storage.googleapis.com' + filename
-                print link
-                if self.request.get('down_link'):
-                    resp['down_link'] = link
-                #return webapp2.redirect('http://storage.googleapis.com' + filename)
-                if self.request.get('link'):
-                    resp['link'] = link
-                self.write(json.dumps(resp))
+        elif self.request.get('url'):
+            img = url_to_img(self.request.get('url'))
+            if not img:
+                self.write('Could not open the image.')
+                return
+        msg = self.request.get('msg')
+        resp = dict()
+        if img and msg:
+            filename = '/obrgjagm1v6rb0/' + str(int(round(time.time() * 1000))) + '.png'
+            # Encodes the text into the image
+            print 'encoding'
+            self.encode(msg, img, self.request.get('key'))
+            print 'just encoded'
+            # Creates a file with the given name
+            self.make_file(filename, img)
+            link = 'http://storage.googleapis.com' + filename
+            if self.request.get('down_link'):
+                resp['down_link'] = link
+            #return webapp2.redirect('http://storage.googleapis.com' + filename)
+            if self.request.get('link'):
+                resp['link'] = link
+            self.write(json.dumps(resp))
 
 class Decode(Handler):
     def grouper(self, n, iterable, padvalue=None):
@@ -110,10 +121,19 @@ class Decode(Handler):
         return izip_longest(*[iter(iterable)]*n, fillvalue=padvalue)
 
     def compare_index(self, current_pixel, colorType, compare_index):
+        if not current_pixel:
+            return True
         val = bin(current_pixel[colorType])
         last_val = val[-1]
         if compare_index<SECRET_LENGTH and last_val != SECRET_KEY[compare_index]:
             return True
+        return False
+
+    def is_done(self, bin_list, size):
+        if size % 8 == 0:
+            if sum(int(a) for a in bin_list[-8:]) == 0:
+                return True
+        return False
 
     def decode(self, image, passwords=[]):
         passwords.insert(0,"password")
@@ -131,7 +151,7 @@ class Decode(Handler):
                     break
                 current_pixel = pix_map[i, j]
                 for k in range(0,3):
-                    if self.compare_index(current_pixel,k, compare_index):
+                    if self.compare_index(current_pixel, k, compare_index):
                             return None
                     compare_index += 1
                     if compare_index > SECRET_LENGTH:
@@ -150,51 +170,87 @@ class Decode(Handler):
             asciiPassword += str(unichr(int(character, 2)))
         binary_message = ""
         binary_message_list=[]
+        cur = 0
         if asciiPassword in passwords:
             for i in range(image.size[0]):
                 for j in range(image.size[1]):
-                        current_pixel = pix_map[i, j]
-                        red_val = bin(current_pixel[0])
-                        green_val = bin(current_pixel[1])
-                        blue_val = bin(current_pixel[2])
-                        last_red_bit = red_val[len(red_val) - 1]
-                        last_green_bit = green_val[len(green_val) - 1]
-                        last_blue_bit = blue_val[len(blue_val) - 1]
-                        binary_message_list.append(last_red_bit)
-                        binary_message_list.append(last_green_bit)
-                        binary_message_list.append(last_blue_bit)
-            del binary_message_list[0:81+passLength+8]
-            binary_message = self.grouper(8, ''.join(binary_message_list), 0)
-            for byte in binary_message:
-                character = ''.join(byte)
-                if character == '00000000':
-                    return message
-                else:
-                    message += str(unichr(int(character, 2)))
+                    current_pixel = pix_map[i, j]
+                    red_val = bin(current_pixel[0])
+                    green_val = bin(current_pixel[1])
+                    blue_val = bin(current_pixel[2])
+                    last_red_bit = red_val[len(red_val) - 1]
+                    last_green_bit = green_val[len(green_val) - 1]
+                    last_blue_bit = blue_val[len(blue_val) - 1]
+                    binary_message_list.append(last_red_bit)
+                    cur += 1
+                    if cur >= 81+passLength+8 and self.is_done(binary_message_list, cur):
+                        del binary_message_list[0:81+passLength+8]
+                        binary_message = self.grouper(8, ''.join(binary_message_list), 0)
+                        for byte in binary_message:
+                            character = ''.join(str(a) for a in byte)
+                            if character == '00000000':
+                                return message
+                            else:
+                                message += str(unichr(int(character, 2)))
+                    binary_message_list.append(last_green_bit)
+                    cur += 1
+                    if cur >= 81+passLength+8 and self.is_done(binary_message_list, cur):
+                        del binary_message_list[0:81+passLength+8]
+                        binary_message = self.grouper(8, ''.join(binary_message_list), 0)
+                        for byte in binary_message:
+                            character = ''.join(str(a) for a in byte)
+                            if character == '00000000':
+                                return message
+                            else:
+                                message += str(unichr(int(character, 2)))
+                    binary_message_list.append(last_blue_bit)
+                    cur += 1
+                    if cur >= 81+passLength+8 and self.is_done(binary_message_list, cur):
+                        del binary_message_list[0:81+passLength+8]
+                        binary_message = self.grouper(8, ''.join(binary_message_list), 0)
+                        for byte in binary_message:
+                            character = ''.join(str(a) for a in byte)
+                            if character == '00000000':
+                                return message
+                            else:
+                                message += str(unichr(int(character, 2)))
+
         else:
             return None
 
-    def url_to_img(self, url):
-        handle = urllib.urlopen(url)
-        return Image.open(BytesIO(bytearray(handle.read())))
-
     def post(self):
         if self.request.get('urls'):
+            print self.request.get('urls')
             urls = json.loads(self.request.get('urls'))
             resp = dict()
             i = 0
+            print "about to print passwords"
+            passwords = []
+            if self.request.get('passwords'):
+                print "printing passwords"
+                passwords = json.loads(self.request.get('passwords'))
+                for password in passwords:
+                    print password
+
             for url in urls:
                 splitUrl = url.split(".")
                 #make sure url ends in .png or .jpg
-                if "png" in splitUrl[-1] or "jpg" in splitUrl[-1] or "jpeg" in splitUrl[-1]:
-                    img = self.url_to_img(url)
-                    resp[url] = self.decode(img)
+                if "png" in splitUrl[-1]:
+                    img = url_to_img(url)
+                    if (img != None):
+                        maybe_msg = self.decode(img, passwords)
+                        if not maybe_msg:
+                            resp[url] = maybe_msg
                     i += 1
             print json.dumps(resp)
+
+
+
             self.write(json.dumps(resp))
 
+
         elif self.request.get('url'):
-            img = self.url_to_img(self.request.get('url'))
+            img = url_to_img(self.request.get('url'))
             msg = self.decode(img, [self.request.get('key')])
             print msg
             self.write(json.dumps(msg))
